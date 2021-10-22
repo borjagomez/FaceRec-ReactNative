9faaac5a552369411d3b61202158eca8d3f00a75
Borja Gómez Muñoz <borja@reverscore.com>
2021-10-22 15:12:16 +0200

    Face Detection ok

--- App.js
+++ App.js
@@ -1,11 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Camera } from 'expo-camera';
+import logo from './assets/rever-logo.jpg';
+import * as FaceDetector from 'expo-face-detector';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [faceDetected, setFaceDetected] = useState(false);
+
+  let camera:Camera;
+  let photo = null;
+  const faceMessage = faceDetected ? 'Face Detected' : 'No Face';
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const takePhoto = async () => {
+    if (camera) {
+      photo = await camera.takePictureAsync();
+    }
+  };
+
+  const handleFacesDetected = (event) => {
+    if (event.faces.length > 0) {
+      const faceY = event.faces[0].bounds.origin.y;
+      const faceHeight = event.faces[0].bounds.size.height;
+      console.log(faceY + ' ' + faceHeight)
+      if (faceY + faceHeight < 300) setFaceDetected(true);
+      else setFaceDetected(false);
+    } else setFaceDetected(false);
+  }
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} style={styles.logo} /> 
+
+      <View>
+        <Camera style={styles.camera} type={type} 
+          ref={ref => {
+            camera = ref;
+          }} 
+          
+          onFacesDetected={handleFacesDetected}
+          
+          faceDetectorSettings={{
+            mode: FaceDetector.Constants.Mode.fast,
+            detectLandmarks: FaceDetector.Constants.Landmarks.none,
+            runClassifications: FaceDetector.Constants.Classifications.none,
+            minDetectionInterval: 100,
+            tracking: true,
+          }}
+        >
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                setType(
+                  type === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back
+                );
+              }}>
+              <Text style={styles.text}> Flip </Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+        <Text style={styles.label}>{faceMessage}</Text>
+      </View>
+
+      <TouchableOpacity
+        onPress={takePhoto}
+        style={styles.buttonCapture}
+        disabled={!faceDetected}>
+        <Text style={styles.buttonText}>Capture</Text>
+      </TouchableOpacity>
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +98,44 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around'
   },
+  logo: {
+    width: 100,
+    height: 100 
+  },
+  camera: {
+    width:300,
+    height: 300
+  },  
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+    width: '100%'
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
+  },
+  label: {
+    fontSize: 18,
+    color: 'black',
+    textAlign: 'center'
+  },
+  buttonCapture: {
+    backgroundColor: "#888",
+    padding: 20,
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  }  
 });
